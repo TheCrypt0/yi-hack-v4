9a2a82eba5fde5729ab91802bbafd00ce3ac2eda
TheCrypt0 <cryptoofficial@gmail.com>
2019-01-09 22:21:09 +0100

    [Add] Added src build scripts and submodules patches
    They will be probably replaced by Makefile in the future

---
+++ src/dropbear/localoptions.h
@@ -0,0 +1,8 @@
+#ifndef DROPBEAR_LOCALOPTIONS_H
+#define DROPBEAR_LOCALOPTIONS_H
+
+#define DSS_PRIV_FILENAME "/home/yi-hack-v4/etc/dropbear/dropbear_dss_host_key"
+#define RSA_PRIV_FILENAME "/home/yi-hack-v4/etc/dropbear/dropbear_rsa_host_key"
+#define ECDSA_PRIV_FILENAME "/home/yi-hack-v4/etc/dropbear/dropbear_ecdsa_host_key"
+
+#endif /* DROPBEAR_LOCALOPTIONS_H */
