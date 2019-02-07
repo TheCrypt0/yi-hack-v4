d93c2ff2f23c08598aac37c3593013bb664aa46e
TheCrypt0 <cryptoofficial@gmail.com>
2019-02-07 20:38:24 +0100

    [Add] Added license text to the scripts

--- scripts/common.sh
+++ scripts/common.sh
@@ -1,11 +1,21 @@
 #!/bin/bash
 
-###############################################################################
-#  common.sh
-###############################################################################
-#  this file is part of yi-hack-v4
-#  https://github.com/TheCrypt0/yi-hack-v4
-###############################################################################
+#
+#  This file is part of yi-hack-v4 (https://github.com/TheCrypt0/yi-hack-v4).
+#  Copyright (c) 2018-2019 Davide Maggioni.
+# 
+#  This program is free software: you can redistribute it and/or modify
+#  it under the terms of the GNU General Public License as published by
+#  the Free Software Foundation, version 3.
+# 
+#  This program is distributed in the hope that it will be useful, but
+#  WITHOUT ANY WARRANTY; without even the implied warranty of
+#  MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the GNU
+#  General Public License for more details.
+# 
+#  You should have received a copy of the GNU General Public License
+#  along with this program. If not, see <http://www.gnu.org/licenses/>.
+#
 
 ###############################################################################
 # Cameras list
