6bc68062f3f51e35965ff2cd99083be0f431fa1c
TheCrypt0 <cryptoofficial@gmail.com>
2019-04-30 15:14:31 +0200

    [Edit] Dynamic configuration backend for the Web Interface

--- src/www/httpd/cgi-bin/get_configs.sh
+++ src/www/httpd/cgi-bin/get_configs.sh
@@ -16,16 +16,17 @@ get_config()
 
 printf "Content-type: application/json\r\n\r\n"
 
-# Yeah, it's pretty ugly.. but hey, it works.
-
 printf "{\n"
-printf "\"%s\":\"%s\",\n" "HTTPD"           "$(get_config HTTPD)"
-printf "\"%s\":\"%s\",\n" "TELNETD"         "$(get_config TELNETD)"
-printf "\"%s\":\"%s\",\n" "SSHD"            "$(get_config SSHD)"
-printf "\"%s\":\"%s\",\n" "FTPD"            "$(get_config FTPD)"
-printf "\"%s\":\"%s\",\n" "PROXYCHAINSNG"   "$(get_config PROXYCHAINSNG)"
-printf "\"%s\":\"%s\",\n" "CHECK_UPDATES"   "$(get_config CHECK_UPDATES)"
-printf "\"%s\":\"%s\",\n" "DISABLE_CLOUD"   "$(get_config DISABLE_CLOUD)"
-printf "\"%s\":\"%s\"\n"  "NTPD"            "$(get_config NTPD)"
+
+while IFS= read -r LINE ; do
+    if [ ! -z $LINE ] ; then
+        printf "\"%s\",\n" $(echo "$LINE" | sed -r 's/=/":"/g') # Format to json and replace = with ":"
+    fi
+done < "$YI_HACK_PREFIX/$CONF_FILE"
+
+printf "\"%s\":\"%s\",\n"  "HOSTNAME" "$(cat /etc/hostname)"
+
+# Empty values to "close" the json
+printf "\"%s\":\"%s\"\n"  "NULL" "NULL"
 
 printf "}"
--- src/www/httpd/cgi-bin/update_configs.sh
+++ src/www/httpd/cgi-bin/update_configs.sh
@@ -1,6 +1,9 @@
 #!/bin/sh
 
+printf "Content-type: application/json\r\n\r\n"
+
 read QUERY_STRING
+
 CONF_FILE="etc/system.conf"
 
 if [ -d "/usr/yi-hack-v4" ]; then
@@ -9,11 +12,31 @@ elif [ -d "/home/yi-hack-v4" ]; then
         YI_HACK_PREFIX="/home/yi-hack-v4"
 fi
 
-echo $QUERY_STRING | tr '&' '\n' > $YI_HACK_PREFIX/$CONF_FILE
+PARAMS=$(echo "$QUERY_STRING" | tr "&" " ")
 
-printf "Content-type: application/json\r\n\r\n"
+for S in $PARAMS ; do
+    PARAM=$(echo "$S" | tr "=" " ")
+    KEY=""
+    VALUE=""
+    
+    for SP in $PARAM ; do
+        if [ -z $KEY ]; then
+            KEY=$SP
+        else
+            VALUE=$SP
+        fi
+    done
+    
+    if [ $KEY == "hostname" ] ; then
+        if [ -z $VALUE ] ; then
+            echo "$VALUE" > /etc/hostname
+        fi
+    else
+        sed -i "s/^\(${KEY}\s*=\s*\).*$/\1${VALUE}/" $YI_HACK_PREFIX/$CONF_FILE
+    fi   
+done
 
-# Yeah, it's pretty ugly.. but hey, it works.
+# Yeah, it's pretty ugly.
 
 printf "{\n"
 printf "\"%s\":\"%s\"\\n" "error" "false"
