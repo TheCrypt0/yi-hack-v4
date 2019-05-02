479be177360ad01f62c8ad19cdff6c0804305fe4
TheCrypt0 <cryptoofficial@gmail.com>
2019-05-02 23:25:04 +0200

    [Edit] Even better configuration backend

--- src/www/httpd/cgi-bin/set_configs.sh
+++ src/www/httpd/cgi-bin/set_configs.sh
@@ -1,18 +1,33 @@
 #!/bin/sh
 
-printf "Content-type: application/json\r\n\r\n"
-
-read QUERY_STRING
-
-CONF_FILE="etc/system.conf"
-
 if [ -d "/usr/yi-hack-v4" ]; then
         YI_HACK_PREFIX="/usr/yi-hack-v4"
 elif [ -d "/home/yi-hack-v4" ]; then
         YI_HACK_PREFIX="/home/yi-hack-v4"
 fi
 
-PARAMS=$(echo "$QUERY_STRING" | tr "&" " ")
+get_conf_type()
+{   
+    CONF="$(echo $QUERY_STRING | cut -d'=' -f1)"
+    VAL="$(echo $QUERY_STRING | cut -d'=' -f2)"
+    
+    if [ $CONF == "conf" ] ; then
+        echo $VAL
+    fi
+}
+
+CONF_TYPE="$(get_conf_type)"
+CONF_FILE=""
+
+if [ $CONF_TYPE == "mqtt" ] ; then
+    CONF_FILE="$YI_HACK_PREFIX/etc/mqttv4.conf"
+else
+    CONF_FILE="$YI_HACK_PREFIX/etc/$CONF_TYPE.conf"
+fi
+
+read POST_DATA
+
+PARAMS=$(echo "$POST_DATA" | tr "&" " ")
 
 for S in $PARAMS ; do
     PARAM=$(echo "$S" | tr "=" " ")
@@ -32,11 +47,13 @@ for S in $PARAMS ; do
             echo "$VALUE" > /etc/hostname
         fi
     else
-        sed -i "s/^\(${KEY}\s*=\s*\).*$/\1${VALUE}/" $YI_HACK_PREFIX/$CONF_FILE
+        sed -i "s/^\(${KEY}\s*=\s*\).*$/\1${VALUE}/" $CONF_FILE
     fi   
 done
 
 # Yeah, it's pretty ugly.
+
+printf "Content-type: application/json\r\n\r\n"
 
 printf "{\n"
 printf "\"%s\":\"%s\"\\n" "error" "false"
