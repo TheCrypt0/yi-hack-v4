0c165cb09b2401c0cb2082deb994746427dedab8
TheCrypt0 <cryptoofficial@gmail.com>
2019-05-06 12:50:40 +0200

    [Edit] Better reboot page with camera online status

--- src/www/httpd/htdocs/js/modules/reboot.js
+++ src/www/httpd/htdocs/js/modules/reboot.js
@@ -5,18 +5,53 @@ APP.reboot = (function ($) {
     var timeoutVar;
 
     function init() {
-        rebootCamera();
+        registerEventHandler();
+        setStatus("Camera is online.");
+    }
+
+    function registerEventHandler() {
+        $(document).on("click", '#button-reboot', function (e) {
+            rebootCamera();
+        });
     }
 
     function rebootCamera() {
+        $('#button-reboot').attr("disabled", true);
         $.ajax({
             type: "GET",
             url: 'cgi-bin/reboot.sh',
             dataType: "json",
             error: function(response) {
                 console.log('error', response);
+                $('#button-reboot').attr("disabled", false);
+            },
+            success: function(data) {
+                setStatus("Camera is rebooting...");
+                waitForBoot();
             }
         });
+    }
+
+    function waitForBoot() {
+        setInterval(function(){
+            $.ajax({
+                url: '/',
+                success: function(data) {
+                    setStatus("Camera is back online, redirecting to home.");
+                    $('#button-reboot').attr("disabled", false);
+                    window.location.href="/";
+                },
+                error: function(data) {
+                    setStatus("Waiting for the camera to come back online...");
+                },
+                timeout: 3000,
+            });
+        }, 3000);
+    }
+
+    function setStatus(text)
+    {
+        $('input[type="text"][data-key="STATUS"]').prop('value', text);
     }
 
     return {
